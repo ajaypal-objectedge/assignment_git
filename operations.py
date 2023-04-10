d1da983a5e085250589d062ce7bd5a095d6c1f55
Ajay <ajaypal2961@gmail.com>
2023-04-10 17:13:18 +0530

    used click and run queries for specific customer,partner

--- operations.py
+++ operations.py
@@ -1,6 +1,8 @@
 from main import engine
 from sqlalchemy import  text
 import logging as log 
+import click 
+
 
 log.basicConfig(filename='dbquerieslog.log',level=log.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -34,14 +36,24 @@ def update_allocated_qty_to_zero_python() :
 
 
 
-def select_partner_customer_list():
-
+def select_partner_customer_list(**dict):
+        '''
         select_query = text(""" SELECT partner_sfdc_account_id as partner, ARRAY_AGG(customer_sfdc_account_id) as customers
-	                        FROM customer_partner_relation_table  
+	                        FROM customer_partner_relation_table 
                                 GROUP BY partner_sfdc_account_id ; """)
-        result = query_execution_function(select_query,True)
+        result = query_execution_function(select_query,True,**dict)
         log.info("data selected")
         return result
+        '''
+
+        stmt = "SELECT partner_sfdc_account_id as partner, ARRAY_AGG(customer_sfdc_account_id) as customers FROM customer_partner_relation_table WHERE 1=1 "
+        if dict:
+                if dict["customer"]:
+                        stmt += f" AND customer_sfdc_account_id IN :customer "
+                if dict["partner"]:
+                        stmt += f"AND partner_sfdc_account_id IN :partner "
+        stmt += "GROUP BY partner_sfdc_account_id "
+        return query_execution_function(text(stmt),True,**dict)
 
 def isPartnerHavingErrorStatus(partner) :
         
@@ -71,7 +83,7 @@ def changePartnerStatus(partner,status) :
 
 def changeCustomerStatus(partner,customer,status):
 
-        query = text("UPDATE customer_partner_relation_table SET customer_status = (:status) WHERE partner_sfdc_account_id = (:partner) AND customer_sfdc_account_id = (:customer)")
+        query = text("UPDATE customer_partner_relation_table SET customer_status = (:status) WHERE partner_sfdc_account_id = :partner AND customer_sfdc_account_id = :customer")
         query_execution_function(query,customer=customer,partner=partner,status=status)
         log.info(f"Changed customer_status to {status} for partner_sfdc_account_id : {partner} and customer_sfdc_account_id : {customer}")
 
@@ -85,29 +97,66 @@ def setAllocatedQtyWithCalculations(customer,partner) :
         else :
                 log.info(f'Values added to subscription table for partner : {partner} , customer : {customer}')
 
-partner_customer_list = select_partner_customer_list()
+def allCustomerReady(partner):
+        query = text("SELECT 1 FROM customer_partner_relation_table WHERE partner_sfdc_account_id = (:partner) AND customer_status <> 'READY' ")
+        result = query_execution_function(query,True,partner=partner)
 
-for each_partner_customer_list in partner_customer_list :
-        partner = each_partner_customer_list[0]
-        customers = each_partner_customer_list[1]
-
-        # check partner error status :
-        if isPartnerHavingErrorStatus(partner) :
-                log.info(f"partner_sfdc_account_id : {partner} is having ERROR_STATUS , so SKIPPING this partner")
-                continue
+        if result == None :
+                return False 
         else :
-                log.info(f"partner_sfdc_account_id : {partner} is NOT having ERROR_STATUS , going to loop over its customers ")
-                changePartnerStatus(partner , 'IN_PROGRESS')
-                for customer in customers :
-                        if isCustomerHavingErrorStatus(customer) :
-                                log.info(f"customer_sfdc_account_id : {customer} is having ERROR_STATUS , so SKIPPING this customer")
-                                continue 
+                return True 
+
+def process_data(**dict) :
+        partner_customer_list = select_partner_customer_list(**dict)
+
+        for each_partner_customer_list in partner_customer_list :
+                partner = each_partner_customer_list[0]
+                customers = each_partner_customer_list[1]
+
+                # check partner error status :
+                if isPartnerHavingErrorStatus(partner) :
+                        log.info(f"partner_sfdc_account_id : {partner} is having ERROR_STATUS , so SKIPPING this partner")
+                        continue
+                else :
+                        log.info(f"partner_sfdc_account_id : {partner} is NOT having ERROR_STATUS , going to loop over its customers ")
+                        changePartnerStatus(partner , 'IN_PROGRESS')
+                        for customer in customers :
+                                if isCustomerHavingErrorStatus(customer) :
+                                        log.info(f"customer_sfdc_account_id : {customer} is having ERROR_STATUS , so SKIPPING this customer")
+                                        continue 
+                                else :
+                                        log.info(f"customer_sfdc_account_id : {customer} is NOT having ERROR_STATUS ")
+                                        changeCustomerStatus(partner,customer,'IN_PROGRESS')
+                                        try :
+                                                setAllocatedQtyWithCalculations(customer,partner) 
+                                        except Exception as e :
+                                                changeCustomerStatus(customer,'ERROR_STATUS')
+                                                log.info(f'got ERROR_STATUS FOR partner : {partner} and customer : {customer}')
+                                                continue 
+                                        else :
+                                                changeCustomerStatus(partner,customer,'READY')
+                        
+                        
+                        # if all customer status is ready mark partner status ready
+                        if allCustomerReady(partner) :
+                                log.info(f"All Customers are READY for partner : {partner}")
+                                changePartnerStatus(partner,'READY')
                         else :
-                                log.info(f"customer_sfdc_account_id : {customer} is NOT having ERROR_STATUS ")
-                                changeCustomerStatus(partner,customer,'IN_PROGRESS')
-                                setAllocatedQtyWithCalculations(customer,partner) 
- 
+                                log.info(f"All Customers are NOT READY for partner : {partner}")
+                break 
 
 
+if __name__ == '__main__' :
+        @click.command()
+        @click.option('-p','--partner',multiple = True , help = 'partner_sfdc_account_id')
+        @click.option('-c','--customer',multiple = True ,help='customer_sfdc_account_id')
+
+        def runner(partner , customer):
+             dict = {
+            'partner' : partner , 
+            'customer': customer
+            }
+             process_data(**dict)
 
 
+runner() 
